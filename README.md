497786345e11cc6b2e0e116759d544324b2da83a
Squagward <cam.n.walter@gmail.com>
2021-02-12 14:41:07 -0600

    Added color customization

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
 # MMBridge
+
 MM Bridge is a module designed to improve the look of the new Money Moves discord bridge.
-In the future, the module will be fully customizable, right now this is just an initial release. You can always change the code to your liking if you don't like the color scheme I currently selected.
+You can always change the code to your liking if you don't like the color scheme I currently selected.
+
 ## Credits
+
 - Inspiration taken from itqxic and his bagchat module
 - Major credit goes towards Senither for developing the bridge and to Thundeee and Vigintillion for our implementation of it
 
 Will probably support Rice Moves if and when they add a bridge.
 
 ### How to change the color scheme
-While for now color changing is not supported by default, you can always manually change in code.
-Guide:
-First you will need to go to the module's file
-```
-%appdata%\.minecraft\config\ChatTriggers\modules\mmbridge
-```
-Then you will need to open "index.js" with any text editor (notepad works fine)
-You will want to focus on this particular part of the code:
-https://cdn.discordapp.com/attachments/764872898481356831/809580765393584158/unknown.png
-Changing "e" to a valid mc color code will change the color of the [BRIDGE]. Changing the "3" to any valid mc color code will change the color of the name.
 
+Type /mmbridge to open the settings
---
+++ index.js
@@ -0,0 +1,69 @@
+import { Setting, SettingsObject } from "../SettingsManager/SettingsManager";
+const settings = new SettingsObject("personal modules",
+  [
+    {
+      name: "Settings",
+      settings: [
+        new Setting.Toggle("Enabled", false),
+        new Setting.TextInput("Bridge Text", "[BRIDGE]"),
+        new Setting.StringSelector("Bridge Color", 0, [
+          "&4Dark Red",
+          "&cRed",
+          "&6Gold",
+          "&eYellow",
+          "&2Dark Green",
+          "&aGreen",
+          "&bAqua",
+          "&3Dark Aqua",
+          "&1Dark Blue",
+          "&9Blue",
+          "&dLight Purple",
+          "&5Dark Purple",
+          "&fWhite",
+          "&7Gray",
+          "&8Dark Gray",
+          "&0Black"
+        ]),
+        new Setting.Toggle("Bridge Bold", false),
+
+        new Setting.StringSelector("Discord User Color", 0, [
+          "&4Dark Red",
+          "&cRed",
+          "&6Gold",
+          "&eYellow",
+          "&2Dark Green",
+          "&aGreen",
+          "&bAqua",
+          "&3Dark Aqua",
+          "&1Dark Blue",
+          "&9Blue",
+          "&dLight Purple",
+          "&5Dark Purple",
+          "&fWhite",
+          "&7Gray",
+          "&8Dark Gray",
+          "&0Black"
+        ]),
+        new Setting.Toggle("Discord User Bold", false),
+      ]
+    }
+  ]
+).setCommand("mmbridge");
+Setting.register(settings);
+
+let toggle, bridgeText, bridgeColor, bridgeBold, discordColor, discordBold;
+
+register("tick", () => {
+  toggle = settings.getSetting("Settings", "Enabled");
+  bridgeText = settings.getSetting("Settings", "Bridge Text");
+  bridgeColor = settings.getSetting("Settings", "Bridge Color").substring(0, 2);
+  bridgeBold = settings.getSetting("Settings", "Bridge Bold") ? "§l" : "";
+  discordColor = settings.getSetting("Settings", "Discord User Color").substring(0, 2);
+  discordBold = settings.getSetting("Settings", "Discord User Bold") ? "§l" : "";
+});
+
+register("chat", (player, msg, event) => {
+  if (!toggle) return;
+  cancel(event);
+  ChatLib.chat(`§2Guild > ${bridgeColor + bridgeBold + bridgeText} ${discordColor + discordBold + player}§r: ${msg}`);
+}).setCriteria("&r&2Guild > &a[VIP] 0oompaL0oompa &6[R]&f: &r${player}: ${msg}&r");
