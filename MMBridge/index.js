e7bbcc24d280bda1a1322af359ff0756961f3a7a
MisterCheezeCake <75341435+MisterCheezeCake@users.noreply.github.com>
2021-02-12 20:47:51 -0500

    Version 1.2.0

--- MMBridge/index.js
+++ MMBridge/index.js
@@ -4,7 +4,7 @@ const settings = new SettingsObject('MMBridge', [
       name: 'Module Settings',
       settings: [
           new Setting.Toggle('Toggle Module', true),
-          new Setting.TextInput("Customize", "&2Guild > &e[BRIDGE]&3"),
+          new Setting.TextInput("Customize", "&e[BRIDGE]&3"),
           new Setting.Button(
             "Do /mmguide for a guide on how to use the customization box",
             " ",
@@ -15,21 +15,33 @@ const settings = new SettingsObject('MMBridge', [
           " ",
           () => {}
       ),
+      new Setting.Button(
+        "To see your changes you must do /mmreload",
+        " ",
+        () => {}
+    ),
       ],
   },
 ]).setCommand('mmbridge').setSize(400, 100);
 
 Setting.register(settings);
+
 let sayText = settings.getSetting('Module Settings', "Customize");
+let refresh = false
+
 register("command", (e) => {
-  ChatLib.chat(sayText + ' Testing 123')
+  ChatLib.chat('&2Guild > ' + sayText + ' Testing 123')
 }).setName("mmbridgetest")
 register("command", (e) => {
-  ChatLib.chat('&6[MMBRIDGE]&f: &rTo customize the module, you need to edit the box in the /mmbridge settings menu. To do this, click on the box and edit the numbers to any valid mc color code. The first one will change the colour of the &2Guild > &r, the second will change the color of the &e[BRIDGE] &rprefix, and the third will change the color of the persons name, do /mmbridgetest to test out your settings. &c&lYou need to /ct load for the settings to update.')
+  ChatLib.chat('&6[MMBRIDGE]&f: &rTo customize the module, you need to edit the box in the /mmbridge settings menu. To do this, click on the box and edit the numbers to any valid mc color code. The first one will change the colour of the &e[BRIDGE]&r prefix and the second one changes the color of the  name. &c&lYou need to /mmreload for the settings to update.')
 }).setName("mmguide")
 register("command", (e) => {
   ChatLib.chat('&11 &22 &33 &44 &55 &66 &77 &88 &99 &00 &aa &bb &cc &dd &ee &ff &ll')
 }).setName("mmcolors")
+register("command", (e) => {
+  sayText = settings.getSetting('Module Settings', "Customize");
+  ChatLib.chat('&aReloaded color settings')
+}).setName("mmreload")
 
 register("chat", function(event) {
   var unformattedMessage = ChatLib.getChatMessage(event);
@@ -37,7 +49,7 @@ register("chat", function(event) {
   if(!settings.getSetting("Module Settings", "Toggle Module")) return;
   if(msgString.startsWith("§2Guild > §a[VIP] 0oompaL0oompa §6[R]§f:")){
     cancel(event)
-    msgString = msgString.replace("§2Guild > §a[VIP] 0oompaL0oompa §6[R]§f: ",sayText + ' ')
+    msgString = msgString.replace("§2Guild > §a[VIP] 0oompaL0oompa §6[R]§f: ",'&2Guild > '+ sayText + ' ')
     msgString = msgString.replace(": ","§f: ")
     ChatLib.chat(msgString)
   }
